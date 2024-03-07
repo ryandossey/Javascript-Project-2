260cfa1734e06c4d1649d076bc3cf7a11bde7d29
Mendis, Steve Collin <smendis@iu.edu>
2024-03-07 15:56:46 -0500

    Initial commit - index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// optionally add a parameter for how large a board to create
+function createBoard() {
+    // reminder: each number button should store the value as a data- property
+}
+
+function guessNumber(event) {} // takes an event from the number being clicked on
+
+function tryAgain() {} // connected to a try again / replay button
+
+function init() {
+    createBoard();
+    // add event listeners here
+}
+init();
